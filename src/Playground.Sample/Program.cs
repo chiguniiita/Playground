8f58754076106fc8a468c18a483569aa343d78da
chiguniiita <chigu.niii.ta@gmail.com>
2017-01-02 23:57:40 +0900

    Test VSO Build Trigger

--- src/Playground.Sample/Program.cs
+++ src/Playground.Sample/Program.cs
@@ -12,7 +12,7 @@ namespace Playground.Sample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world!!");
             Console.WriteLine(GetInformationalVersion());
             Console.ReadKey();
         }
