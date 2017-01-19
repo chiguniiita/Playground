87b8b0eda0b151e44e994ee6256a6b2e0ce29b70
chiguniiita <chigu.niii.ta@gmail.com>
2017-01-20 01:20:33 +0900

    fix #4
    support workspace
    
    support workspace path

--- embed_commit_hash.ps1
+++ embed_commit_hash.ps1
@@ -1,8 +1,13 @@
-$ASSEMBLY_INFO_PATH = "$env:SYSTEM_DEFAULTWORKINGDIRECTORY\src\Playground.Sample\Properties\AssemblyInfo.cs"
+Param([string]$WORKSPACE, [string]$GIT_COMMIT_HASH)
+
+Write-Host $WORKSPACE
+Write-Host $GIT_COMMIT_HASH
+
+$ASSEMBLY_INFO_PATH = "$WORKSPACE\src\Playground.Sample\Properties\AssemblyInfo.cs"
 $ASSEMBLY_INFO_CONTENT = Get-Content $ASSEMBLY_INFO_PATH
 
 $ASSEMBLY_VERSION = [regex]::Match($ASSEMBLY_INFO_CONTENT, "AssemblyVersion\(`"(.+?)`"\)").Groups[1].Value
-$ASSEMBLY_INFORMATIONAL_VERSION = [string]::Format("[assembly: AssemblyInformationalVersion(`"{0}-{1}`")]", $ASSEMBLY_VERSION, $env:BUILD_SOURCEVERSION.Substring(0, 7))
+$ASSEMBLY_INFORMATIONAL_VERSION = [string]::Format("[assembly: AssemblyInformationalVersion(`"{0}-{1}`")]", $ASSEMBLY_VERSION, $GIT_COMMIT_HASH.Substring(0, 7))
 
 # 取得できたバージョンを表示
 Write-Host $ASSEMBLY_INFORMATIONAL_VERSION
